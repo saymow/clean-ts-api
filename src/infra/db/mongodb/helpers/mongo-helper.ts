f459a405034713d99c979192cbe74476b2caea08
Gustavo <gustavo.4lv5s@gmail.com>
2021-02-16 17:56:57 -0300

    refactor: move mappinng to MongoHelper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -16,5 +16,10 @@ export const MongoHelper = {
 
   getCollection (name: string): Collection {
     return this.client.db().collection(name)
+  },
+
+  map (document: any): any {
+    const { _id, ...documentWithoutId } = document
+    return Object.assign({}, documentWithoutId, { id: _id })
   }
 }
