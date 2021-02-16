96e2ff954e527c6eb90262cc4241b7167c6cc453
Gustavo <gustavo.4lv5s@gmail.com>
2021-02-16 17:43:19 -0300

    fix: jest-mongodb version issue

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   mongodbMemoryServerOptions: {
     binary: {
-      version: '4.0.3',
+      version: 'latest',
       skipMD5: true
     },
-    autoStart: false,
     instance: {
       dbName: 'jest'
-    }
+    },
+    autoStart: false
   }
 }
