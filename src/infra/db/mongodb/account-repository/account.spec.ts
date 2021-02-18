fe9676d503a1bcdddacd1e0e803e319ab22eb687
Gustavo <gustavo.4lv5s@gmail.com>
2021-02-17 23:56:00 -0300

    feat: ensure mongodb reconnect if client is down

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -10,7 +10,7 @@ afterAll(async () => {
 })
 
 beforeEach(async () => {
-  const accountCollection = MongoHelper.getCollection('accounts')
+  const accountCollection = await MongoHelper.getCollection('accounts')
   await accountCollection.deleteMany({})
 })
 
