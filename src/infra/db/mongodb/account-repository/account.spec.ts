5b3892be79b803856392d29003d1f94a0afc8bdd
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2021-12-05 11:27:48 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,28 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account'
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    // environment variable set up by @shelf/jest-mongodb in-memory database
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should return an account on success', async () => {
+    const sut = new AccountMongoRepository()
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+
+    expect(account).toBeDefined()
+    expect(account.id).toBeDefined()
+    expect(account.name).toEqual('any_name')
+    expect(account.email).toEqual('any_email@mail.com')
+    expect(account.password).toEqual('any_password')
+  })
+})
