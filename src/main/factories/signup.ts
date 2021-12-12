3be13660089b4bfeca485e5a8e80ae4edffa6c31
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2021-12-12 09:05:45 -0300

    feat: ensure SignUpController calls Validation with correct values

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -3,6 +3,7 @@ import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
 import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
 import { SignUpController } from '../../presentation/controllers/signup/signup'
+import { Validation } from '../../presentation/helpers/validators/validation'
 import { Controller } from '../../presentation/protocols'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { LogControllerDecorator } from '../decorators/log'
@@ -13,7 +14,8 @@ export const makeSignUpController = (): Controller => {
   return new LogControllerDecorator(
     new SignUpController(
       new EmailValidatorAdapter(),
-      new DbAddAccount(new BcryptAdapter(salt), new AccountMongoRepository())
+      new DbAddAccount(new BcryptAdapter(salt), new AccountMongoRepository()),
+      { validate: (input: any): Error => null } as Validation
     ),
     new LogMongoRepository()
   )
