ef077662ddcc0772e0e3ffa80dcd865ebe89cbed
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2021-12-14 20:52:02 -0300

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,8 +1,10 @@
 import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { hash } from 'bcrypt'
 
 describe('Login Routes', () => {
+  let accountCollection
   beforeAll(async () => {
     // environment variable set up by @shelf/jest-mongodb in-memory database
     await MongoHelper.connect(process.env.MONGO_URL)
@@ -13,7 +15,7 @@ describe('Login Routes', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -22,6 +24,22 @@ describe('Login Routes', () => {
       await request(app)
         .post('/api/signup')
         .send({ name: 'Gustavo', email: 'gustavo_alves2010@yahoo.com.br', password: '123', passwordConfirmation: '123' })
+        .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('Should return 200 on login', async () => {
+      const password = await hash('123', 12)
+      await accountCollection.insertOne({
+        name: 'Gustavo',
+        email: 'gustavo_alves2010@yahoo.com.br',
+        password
+      })
+
+      await request(app)
+        .post('/api/login')
+        .send({ email: 'gustavo_alves2010@yahoo.com.br', password: '123' })
         .expect(200)
     })
   })
