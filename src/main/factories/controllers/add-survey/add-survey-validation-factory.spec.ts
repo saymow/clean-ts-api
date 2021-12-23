d1dfe0971ccce8c58f06e2aa96e3b4083dbe390c
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2021-12-23 20:25:36 -0300

    feat: add AddSurvey composition

---
+++ src/main/factories/controllers/add-survey/add-survey-validation-factory.spec.ts
@@ -0,0 +1,14 @@
+import { ValidationComposite, RequiredFieldValidation } from '../../../../validation/validators'
+import { Validation } from '../../../../presentation/protocols/validation'
+import { makeAddSurveyValidation } from './add-survey-validation-factory'
+
+jest.mock('../../../../validation/validators/validators-composite')
+
+describe('AddSurveyValidation factory', () => {
+  test('Should call ValidationComposite with all validations', () => {
+    makeAddSurveyValidation()
+    const validations: Validation[] = ['question', 'answers'].map((field) => new RequiredFieldValidation(field))
+
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
---
+++ src/main/routes/survey-routes.test.ts
@@ -0,0 +1,40 @@
+import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import app from '../config/app'
+
+describe('Survey Routes', () => {
+  let surveyCollection
+  beforeAll(async () => {
+    // environment variable set up by @shelf/jest-mongodb in-memory database
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+  })
+
+  describe('POST /surveys', () => {
+    test('Should return 204 on add survey success', async () => {
+      await request(app)
+        .post('/api/surveys')
+        .send({
+          question: 'question',
+          answers: [
+            {
+              answer: 'Answer 1',
+              images: 'https://image-name.com'
+            },
+            {
+              answer: 'Answer 2'
+            }
+          ]
+        })
+        .expect(204)
+    })
+  })
+})
