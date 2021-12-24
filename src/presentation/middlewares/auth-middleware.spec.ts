c0e8a3d67559889e20c3ffee592780f466483517
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2021-12-23 21:31:51 -0300

    refactor: move sut creation to a factory

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -11,27 +11,37 @@ const makeFakeAccount = (): AccountModel => ({
   password: 'hashed_password'
 })
 
+interface SutTypes {
+  sut: AuthMiddleware
+  loadAccountByTokenStub: LoadAccountByToken
+}
+
+const makeLoadAccountByTokenStub = (): LoadAccountByToken => {
+  class LoadAccountByTokenStub implements LoadAccountByToken {
+    async execute (token: string): Promise<AccountModel> {
+      return await Promise.resolve(makeFakeAccount())
+    }
+  }
+
+  return new LoadAccountByTokenStub()
+}
+
+const makeSut = (): SutTypes => {
+  const loadAccountByTokenStub = makeLoadAccountByTokenStub()
+  const sut = new AuthMiddleware(loadAccountByTokenStub)
+
+  return { sut, loadAccountByTokenStub }
+}
+
 describe('Auth Middleware', () => {
   test('Should return 401 if no x-access-token exists in headers', async () => {
-    class LoadAccountByTokenStub implements LoadAccountByToken {
-      async execute (token: string): Promise<AccountModel> {
-        return await Promise.resolve(makeFakeAccount())
-      }
-    }
-    const loadAccountByTokenStub = new LoadAccountByTokenStub()
-    const sut = new AuthMiddleware(loadAccountByTokenStub)
+    const { sut } = makeSut()
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(unauthorized())
   })
 
   test('Should call LoadAccountByToken with correct accessToken', async () => {
-    class LoadAccountByTokenStub implements LoadAccountByToken {
-      async execute (token: string): Promise<AccountModel> {
-        return await Promise.resolve(makeFakeAccount())
-      }
-    }
-    const loadAccountByTokenStub = new LoadAccountByTokenStub()
-    const sut = new AuthMiddleware(loadAccountByTokenStub)
+    const { sut, loadAccountByTokenStub } = makeSut()
     const loadSpy = jest.spyOn(loadAccountByTokenStub, 'execute')
     await sut.handle({
       headers: {
