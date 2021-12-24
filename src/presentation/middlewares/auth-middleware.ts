8ea96dd5a78c7b732d63d476cd598fe92bf283ec
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2021-12-23 21:09:02 -0300

    feat: ensure AuthMiddleware returns 401 if no header token is found

---
+++ src/presentation/middlewares/auth-middleware.ts
@@ -0,0 +1,8 @@
+import { unauthorized } from '../helpers/http/http-helper'
+import { HttpRequest, HttpResponse, Middleware } from '../protocols'
+
+export class AuthMiddleware implements Middleware {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    return unauthorized()
+  }
+}
