14cbb9bad1de41af34b37a651b88193bc73c97d0
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2021-12-26 11:55:51 -0300

    refactor: move sut creation to a factory method

--- src/presentation/controllers/survey/load-surveys/load-surveys.spec.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys.spec.ts
@@ -3,43 +3,47 @@ import { SurveyModel } from '../../../../domain/models/survey'
 import { LoadSurveys } from '../../../../domain/usecases/load-surveys'
 import { LoadSurveysController } from './load-survey'
 
+const makeLoadSurveys = (): LoadSurveys => {
+  class LoadSurveysStub implements LoadSurveys {
+    async execute (): Promise<SurveyModel[]> {
+      return []
+    }
+  }
+
+  return new LoadSurveysStub()
+}
+
+interface SutTypes {
+  sut: LoadSurveysController
+  loadSurveysStub: LoadSurveys
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveysStub = makeLoadSurveys()
+  const sut = new LoadSurveysController(loadSurveysStub)
+
+  return { sut, loadSurveysStub }
+}
+
 describe('LoadSurveys Controller', () => {
   it('Should call LoadSurveys', async () => {
-    class LoadSurveysStub implements LoadSurveys {
-      async execute (): Promise<SurveyModel[]> {
-        return []
-      }
-    }
-    const loadSurveyStub = new LoadSurveysStub()
-    const sut = new LoadSurveysController(loadSurveyStub)
-    const loadSpy = jest.spyOn(loadSurveyStub, 'execute')
+    const { sut, loadSurveysStub } = makeSut()
+    const loadSpy = jest.spyOn(loadSurveysStub, 'execute')
     await sut.handle({})
 
     expect(loadSpy).toHaveBeenCalledTimes(1)
   })
 
   it('Should return 500 if LoadSurveys throws', async () => {
-    class LoadSurveysStub implements LoadSurveys {
-      async execute (): Promise<SurveyModel[]> {
-        return []
-      }
-    }
-    const loadSurveyStub = new LoadSurveysStub()
-    const sut = new LoadSurveysController(loadSurveyStub)
-    jest.spyOn(loadSurveyStub, 'execute').mockImplementationOnce(() => { throw new Error() })
+    const { sut, loadSurveysStub } = makeSut()
+    jest.spyOn(loadSurveysStub, 'execute').mockImplementationOnce(() => { throw new Error() })
     const httpResponse = await sut.handle({})
 
     expect(httpResponse).toEqual(serverError(new Error()))
   })
 
   it('Should return 200 on success', async () => {
-    class LoadSurveysStub implements LoadSurveys {
-      async execute (): Promise<SurveyModel[]> {
-        return []
-      }
-    }
-    const loadSurveyStub = new LoadSurveysStub()
-    const sut = new LoadSurveysController(loadSurveyStub)
+    const { sut } = makeSut()
     const httpResponse = await sut.handle({})
 
     expect(httpResponse).toEqual(ok([]))
