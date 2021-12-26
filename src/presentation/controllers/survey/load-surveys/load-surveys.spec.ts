7219bce72e4cac7a22ce93a4df5eb73a5172b331
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2021-12-26 11:50:21 -0300

    test: ensure LoadSurveyController retuns 500 if LoadSurveys throws

--- src/presentation/controllers/survey/load-surveys/load-surveys.spec.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys.spec.ts
@@ -1,3 +1,4 @@
+import { serverError } from '../../../helpers/http/http-helper'
 import { SurveyModel } from '../../../../domain/models/survey'
 import { LoadSurveys } from '../../../../domain/usecases/load-surveys'
 import { LoadSurveysController } from './load-survey'
@@ -15,5 +16,19 @@ describe('LoadSurveys Controller', () => {
     await sut.handle({})
 
     expect(loadSpy).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should return 500 if LoadSurveys throws', async () => {
+    class LoadSurveysStub implements LoadSurveys {
+      async execute (): Promise<SurveyModel[]> {
+        return []
+      }
+    }
+    const loadSurveyStub = new LoadSurveysStub()
+    const sut = new LoadSurveysController(loadSurveyStub)
+    jest.spyOn(loadSurveyStub, 'execute').mockImplementationOnce(() => { throw new Error() })
+    const httpResponse = await sut.handle({})
+
+    expect(httpResponse).toEqual(serverError(new Error()))
   })
 })
