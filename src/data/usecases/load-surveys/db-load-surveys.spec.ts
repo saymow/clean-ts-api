7c0eee16c4f5ac40806f2c930080dcbad2c0af2f
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2021-12-27 20:08:45 -0300

    test: ensure DbLoadSurveys calls LoadSurveysRepository once

---
+++ src/data/usecases/load-surveys/db-load-surveys.spec.ts
@@ -0,0 +1,60 @@
+import { LoadSurveysRepository } from '../../protocols/db/survey/load-survey-repository'
+import { SurveyModel } from '../../../domain/models/survey'
+import { DbLoadSurveys } from './db-load-surveys'
+import mockDate from 'mockdate'
+
+const makeFakeSurveys = (): SurveyModel[] => ([
+  {
+    id: 'any_id',
+    question: 'any question',
+    answers: [
+      {
+        answer: 'any_answer 1',
+        image: 'any_image 1'
+      },
+      {
+        answer: 'any_answer 2'
+      }
+    ],
+    date: new Date()
+  },
+  {
+    id: 'any_id 2',
+    question: 'any question 2',
+    answers: [
+      {
+        answer: 'any_answer 1',
+        image: 'any_image 1'
+      },
+      {
+        answer: 'any_answer 2'
+      }
+    ],
+    date: new Date()
+  }
+])
+
+describe('DbLoadSurveys', () => {
+  beforeAll(() => {
+    mockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    mockDate.reset()
+  })
+
+  test('Should call LoadSurveysRepository once', async () => {
+    class LoadSurveysRepositoryStub implements LoadSurveysRepository {
+      async load (): Promise<SurveyModel[]> {
+        return makeFakeSurveys()
+      }
+    }
+    const loadSurveysRepositoryStub = new LoadSurveysRepositoryStub()
+    const loadSpy = jest.spyOn(loadSurveysRepositoryStub, 'load')
+    const sut = new DbLoadSurveys(loadSurveysRepositoryStub)
+
+    await sut.execute()
+
+    expect(loadSpy).toHaveBeenCalledTimes(1)
+  })
+})
