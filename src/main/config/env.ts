01ee1d0079a26202c227e130f5bf403c25196576
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2021-12-30 20:26:27 -0300

    refator: allow api to run wheter on docker or locally

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  MONGO_URL: process.env.MONGO_URL ?? 'mongodb://mongo:27017/clean-node-api',
+  MONGO_URL: process.env.MONGO_URL ?? 'mongodb://localhost:27017/clean-node-api',
   PORT: process.env.PORT ?? 3333,
   JWT_SECRET: process.env.JWT_SECRET ?? 'dsadDASD456=323D[~~D'
 }
