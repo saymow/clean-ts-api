ace7fdc734d80ff55d73e26ed61d78f783c671fe
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2021-12-31 20:26:39 -0300

    feat: ensure DbLoadSurveyById calls LoadSurveyByIdRespository with correct id

--- src/domain/usecases/load-survey-by-id.ts
+++ src/domain/usecases/load-survey-by-id.ts
@@ -1,5 +1,5 @@
 import { SurveyModel } from '@/domain/models/survey'
 
 export interface LoadSurveyById {
-  execute: () => Promise<SurveyModel>
+  execute: (id: string) => Promise<SurveyModel>
 }
--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,7 +1,7 @@
 import { SurveyModel } from '@/domain/models/survey'
 import { AddSurveyModel } from '@/domain/usecases/add-survey'
 import { AddSurveyRepository } from '@/data/protocols/db/survey/add-survey-repository'
-import { LoadSurveysRepository } from '@/data/protocols/db/survey/load-survey-repository'
+import { LoadSurveysRepository } from '@/data/protocols/db/survey/load-surveys-repository'
 import { MongoHelper } from '../helpers/mongo-helper'
 
 export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
