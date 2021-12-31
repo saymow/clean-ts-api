7c1ed3b7d81a42f498e53fcfd6d726ca7a0fbf20
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2021-12-31 20:39:35 -0300

    refactor: move DbLoadSurveyById protocols to their own file

--- src/data/usecases/load-survey-by-id/db-load-survey-by-id.ts
+++ src/data/usecases/load-survey-by-id/db-load-survey-by-id.ts
@@ -1,11 +1,9 @@
-import { LoadSurveyByIdRepository } from '@/data/protocols/db/survey/load-survey-by-id-repository'
-import { SurveyModel } from '@/domain/models/survey'
-import { LoadSurveyById } from '@/domain/usecases/load-survey-by-id'
+import { SurveyModel, LoadSurveyById, LoadSurveyByIdRepository } from './db-load-survey-by-id-protocols'
 
 export class DbLoadSurveyById implements LoadSurveyById {
   constructor (
     private readonly loadSurveyByIdRepository: LoadSurveyByIdRepository
-  ) {}
+  ) { }
 
   async execute (id: string): Promise<SurveyModel> {
     return await this.loadSurveyByIdRepository.loadById(id)
