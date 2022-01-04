10eb6439094a636975d28a07c63e0f3b220f8eab
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2022-01-04 19:43:06 -0300

    feat: ensure SaveSurveyResultController returns 403 if an invalid answer is provided

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -6,15 +6,21 @@ import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 export class SaveSurveyResultController implements Controller {
   constructor (
     private readonly loadSurveyById: LoadSurveyById
-  ) {}
+  ) { }
 
-  async handle (httpRequest: HttpRequest): Promise<HttpResponse > {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const { surveyId } = httpRequest.params
+      const { params: { surveyId }, body: { answer } } = httpRequest
       const survey = await this.loadSurveyById.execute(surveyId)
 
       if (!survey) {
         return forbidden(new InvalidParamError('surveyId'))
+      }
+
+      const surveyAnswer = survey.answers.find(_surveyAnswer => _surveyAnswer.answer === answer)
+
+      if (!surveyAnswer) {
+        return forbidden(new InvalidParamError('answer'))
       }
 
       return null
