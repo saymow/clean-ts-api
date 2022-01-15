38cf463cfc076e34438de829c7d74b66d7975376
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2022-01-15 15:00:35 -0300

    refactor: move LoadSurveyResultController protocols exportation to their own file

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
@@ -1,8 +1,6 @@
-import { LoadSurveyResult } from '@/domain/usecases/survey-result/load-survey-result'
-import { LoadSurveyById } from '@/domain/usecases/survey/load-survey-by-id'
+import { LoadSurveyResult, LoadSurveyById, Controller, HttpRequest, HttpResponse } from './load-survey-result-controller-protocols'
 import { InvalidParamError } from '@/presentation/errors'
 import { forbidden, ok, serverError } from '@/presentation/helpers/http/http-helper'
-import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 
 export class LoadSurveyResultController implements Controller {
   constructor (
