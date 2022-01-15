5fd496f23905c15e4603fc77b468898f9bb5ac5b
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2022-01-15 17:15:03 -0300

    feat: server static files

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,11 +1,13 @@
 import setupMiddlewares from './middlewares'
 import setupSwagger from './config-swagger'
+import setupStaticFiles from './static-files'
 import setupRoutes from './routes'
 import express from 'express'
 
 const app = express()
 
 setupSwagger(app)
+setupStaticFiles(app)
 setupMiddlewares(app)
 void setupRoutes(app)
 
