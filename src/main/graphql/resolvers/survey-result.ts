f8d388f719e0d3fbe2ad806d21b7cb25a0a90fa1
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2022-01-29 15:16:31 -0300

    feat: add SurveyResult query and mutation

---
+++ src/main/graphql/resolvers/survey-result.ts
@@ -0,0 +1,12 @@
+import { adaptResolver } from '@/main/adapters/apollo-server-resolver-adapter'
+import { makeLoadSurveyResultController } from '@/main/factories/controllers/survey-result/load-survey-result/load-survey-result-controller-factory'
+import { makeSaveSurveyResultController } from '@/main/factories/controllers/survey-result/save-survey-result/save-survey-result-controller-factory'
+
+export default {
+  Query: {
+    surveyResult: async (parent: any, args: any, ctx: any) => await adaptResolver(makeLoadSurveyResultController(), args, ctx)
+  },
+  Mutation: {
+    saveSurveyResult: async (parent: any, args: any, ctx: any) => await adaptResolver(makeSaveSurveyResultController(), args, ctx)
+  }
+}
